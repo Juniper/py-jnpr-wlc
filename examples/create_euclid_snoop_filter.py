94c88b6bfed04885e43277308690dca6690adada
trmccart <tim.mccarthy@gmail.com>
2013-10-10 16:08:07 -0700

    example of snoop filter creation and mapping

---
+++ examples/create_euclid_snoop_filter.py
@@ -0,0 +1,74 @@
+import demowlcutils
+from demowlcutils import ppxml, WLC_login
+from pprint import pprint as pp 
+from lxml.builder import E
+from lxml import etree
+from jinja2 import Template
+
+
+wlc = WLC_login()
+rpc = wlc.RpcMaker('set')
+
+
+# for this example we'll build the Euclid compatible snoop filter pointing to the beta 
+# portal IP address. We'll map the snoop filter to the 'default' radio profile. 
+
+radioprofile = 'default'
+euclid_ip = '23.23.125.65'
+
+
+snoop_filter_j2 = Template(u"""
+  <SNOOP-FILTER-TABLE>
+    <SNOOP-FILTER name="EuclidConnector" enable="YES">
+      <SNOOP-OBSERVER-REF target="{{ ip }}"/>
+      <SNOOP-CONDITION>
+        <SNOOP-CONDITION-DIRECTION operation="EQ" direction="RECEIVE"/>
+      </SNOOP-CONDITION>
+      <SNOOP-CONDITION>
+        <SNOOP-CONDITION-FRAME-TYPE operation="EQ" frame-type="PROBE"/>
+      </SNOOP-CONDITION>
+      <SNOOP-CONDITION>
+        <SNOOP-CONDITION-TRANSMITTER-TYPE operation="NEQ" transmitter-type="MEMBER-AP"/>
+      </SNOOP-CONDITION>
+    </SNOOP-FILTER>
+  </SNOOP-FILTER-TABLE>
+  """)
+  
+  
+snoop_observer_j2 = Template(u"""  
+  <SNOOP-OBSERVER-TABLE>
+    <SNOOP-OBSERVER2 target="{{ ip }}" snap-length="0" inter-frame-gap-ms="30000" 
+      transmission-mode="tzsp"/>
+  </SNOOP-OBSERVER-TABLE>
+""")
+
+
+# we need to grab the existing radio profiles so we can add our snoop filter to it 
+# without trashing existing config.
+#
+# Fyi, Snoop filters can also be applied to specific to radios, we use the radio profile 
+# here for convenience 
+# 
+config = wlc.rpc.get_configuration()
+
+# grab the radio profile we're interested in
+rp = config.find(".//RADIO-PROFILE[@name='%s']" % (radioprofile))
+
+
+# create the NSYSTEM object and add our snoop settings
+rpc.data = E('NSYSTEM')
+rpc.data.append(etree.XML(snoop_filter_j2.render( ip=euclid_ip )))
+rpc.data.append(etree.XML(snoop_observer_j2.render( ip=euclid_ip )))
+
+
+# create the Snoop filter mapping and add it to the existing radio profile config
+rp.append(E('SNOOP-FILTER-REF', {'name':'EuclidConnector'}))
+rpc.data.append(rp)
+
+r = rpc()
+
+
+# we've finished creating the filter and mapping it, save config
+wlc.rpc.act_write_configuration()
+
+
