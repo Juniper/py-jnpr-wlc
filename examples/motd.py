ada53bdffbd790a02d437cc2938fc7c77c496ada
Jeremy Schulman <jeremy@dark-matter.co>
2013-10-11 07:44:50 -0400

    updates

--- examples/motd.py
+++ examples/motd.py
@@ -4,11 +4,16 @@ from pprint import pprint as pp
 
 wlc = WLC_login()
 
-### just let the user play around with the wlc object
-### from the python interpreter
+# sometimes you need to set a configuration element that has a text value,
+# rather than element attributes.  this example shows you a technique for
+# doing this; the equivalent of:
+#
+#> set banner motd "text"
 
 rpc = wlc.RpcMaker('set')
 rpc.data = 'MOTD'
 rpc.data.text = "This is a new message, yo!"
 
+# if you want to clear the MOTD, you need to set the :text: to None
 
+rsp = rpc()
