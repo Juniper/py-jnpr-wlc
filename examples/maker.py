65949a026205008c8224c4ad2fa8ca7e82ea11e9
Jeremy Schulman <jeremy@dark-matter.co>
2013-11-14 07:23:36 -0500

    updates

--- examples/maker.py
+++ examples/maker.py
@@ -2,13 +2,13 @@ import demowlcutils
 from demowlcutils import ppxml, WLC_login
 from pprint import pprint as pp 
 
-from jnprwlc import WirelessLanController as WLC
+from jnpr.wlc import WirelessLanController as WLC
 
 wlc = WLC(host='a', user='b', password='c')
 
 r = wlc.RpcMaker( target='vlan', name='Jeremy')
 
-# you can access the following attributes, refer to the jnprwlc.builder
+# you can access the following attributes, refer to the jnpr.wlc.builder
 # file for more details
 
 # r.cmd
